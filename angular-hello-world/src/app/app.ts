18880724f0bd71b999e41c3691e15663351e379f
basarab-daniil <basarab.daniil@iisgalvanimi.edu.it>
2025-10-02 08:46:43 +0000

    commit finale

---
+++ angular-hello-world/src/app/app.ts
@@ -0,0 +1,12 @@
+import { Component, signal } from '@angular/core';
+import { HelloWorld } from './hello-world/hello-world';
+
+@Component({
+  selector: 'app-root',
+  imports: [HelloWorld],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = 'angular-hello-world';
+}
